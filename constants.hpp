a5fc4938211ed1d9101403654abe828cc9f2577d
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-09-21 21:06:35 -0700

    Add display logic

---
+++ constants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <cstddef>
+
+namespace maxwell {
+// Constants for display size and scale factor in pixels
+constexpr int DISPLAY_WIDTH = 64;
+constexpr int DISPLAY_HEIGHT = 32;
+constexpr int SCALE_FACTOR = 20;
+} // namespace maxwell
